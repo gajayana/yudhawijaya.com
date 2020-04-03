226d7b5a94ed852ed5c66473940390a3d14a76fa
Yosef Yudha Wijaya <yosef@yudhawijaya.com>
2020-04-03 18:41:00 +0700

    worked on metas

--- pages/index.vue
+++ pages/index.vue
@@ -21,16 +21,30 @@ export default {
   async asyncData({app, isDev, route, store, env, params, query, req, res, redirect, error}) {
     if(!store.state.home.raw) store.dispatch('home/fetch', { app })
   },
+  head() {
+    return {
+      title: this.metas.title,
+      meta: [
+        { hid: 'description', name: 'description', content: this.metas.description },
+        { hid: 'og:description', name: 'og:description', property: 'og:description', content: this.metas.og_description },
+        { hid: 'og:image', name: 'og:image', property: 'og:image', content: this.metas.og_image },
+        { hid: 'og:title', name: 'og:title', property: 'og:title', content: this.metas.og_title },
+        { hid: 'og:url', name: 'og:url', property: 'og:url', content: 'https://yudhawijaya.com/' },
+        { hid: 'twitter:card', name: 'twitter:card', property: 'twitter:card', content: 'summary' },
+        { hid: 'twitter:creator', name: 'twitter:creator', property: 'twitter:creator', content: '@tuan_yudha' },
+        { hid: 'twitter:site', name: 'twitter:site', property: 'twitter:site', content: '@tuan_yudha' },
+      ],
+    }
+  },
   computed: {
     ...mapGetters({
+      metas: 'home/metas',
       teaser: 'home/teaser',
     }),
     ...mapState({
       error: state => state.home.error,
-      // raw: state => state.home.raw,
     }),
     socialAccounts() {
-      // if (!this.teaser) return
       const accounts = this.teaser.social_accounts.split('||')
 
       return accounts.map((ob) => {
