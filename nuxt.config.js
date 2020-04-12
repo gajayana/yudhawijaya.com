03f4bbf08b947f7dedfa5ca1f9cdf117895dfd74
Yosef Yudha Wijaya <yosef@yudhawijaya.com>
2020-04-12 17:37:35 +0700

    added scroll directive

--- nuxt.config.js
+++ nuxt.config.js
@@ -29,6 +29,7 @@ module.exports = {
   ** Plugins to load before mounting the App
   */
   plugins: [
+    '~/plugins/directives/scroll'
   ],
   /*
   ** Nuxt.js dev-modules
---
+++ plugins/directives/scroll.js
@@ -0,0 +1,10 @@
+import Vue from 'vue'
+
+Vue.directive('scroll', {
+  inserted: (el, binding) => {
+    let f = (evt) => {
+      if (binding.value(evt, el)) window.removeEventListener('scroll', f)
+    }
+    window.addEventListener('scroll', f)
+  }
+})
