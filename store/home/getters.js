89a9cd42c84bdb0fa984320a30b48f4c967f9265
Yosef Yudha Wijaya <yosef@yudhawijaya.com>
2020-04-17 21:59:07 +0700

    simplified store

--- store/home/getters.js
+++
@@ -1,9 +0,0 @@
-export default {
-  metas(state) {
-    const res = state.raw.story.content.body.find(ob => ob.component === 'meta')
-    return res.page_metas
-  },
-  teaser(state) {
-    if (state.raw) return state.raw.story.content.body.find(ob => ob.component === 'teaser')
-  }
-}
