cfd947c0a13dd669fa4cde634721b3f538756b9b
Yosef Yudha Wijaya <yosef@yudhawijaya.com>
2020-04-17 21:59:40 +0700

    updated nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,7 +9,10 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' },
+      { hid: 'twitter:card', name: 'twitter:card', property: 'twitter:card', content: 'summary' },
+      { hid: 'twitter:creator', name: 'twitter:creator', property: 'twitter:creator', content: '@tuan_yudha' },
+      { hid: 'twitter:site', name: 'twitter:site', property: 'twitter:site', content: '@tuan_yudha' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
@@ -54,7 +57,12 @@ module.exports = {
   modules: [
     // Doc: https://axios.nuxtjs.org/usage
     '@nuxtjs/axios',
-    '@nuxtjs/dotenv',
+    [
+      '@nuxtjs/dotenv',
+      {
+        only: []
+      }
+    ],
     '@nuxtjs/pwa',
     [
       'storyblok-nuxt',
@@ -103,5 +111,9 @@ module.exports = {
     */
     extend (config, ctx) {
     }
-  }
+  },
+
+  serverMiddleware: [
+    '~/api/index.js',
+  ]
 }
