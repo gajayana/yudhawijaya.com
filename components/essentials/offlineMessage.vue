e94c00b4137fee495904918af00b2e036d80be66
Yosef Yudha Wijaya <yosef@yudhawijaya.com>
2020-10-14 22:44:03 +0700

    added offline message

---
+++ components/essentials/offlineMessage.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="relative">
+    <div class="bg-red-500 fixed font-serif leading-none px-8 py-2 text-sm text-white bottom-0 right-0">{{ message }}</div>
+  </div>
+</template>
+<script>
+import { mapState } from 'vuex'
+export default {
+  name: 'AppOfflineMessage',
+  computed: {
+    ...mapState({
+      lang: state => state.locale.lang
+    }),
+    message() {
+      return this.lang === 'en' ? 'You are offline' : 'Anda di luar jaringan'
+    }
+  }
+}
+</script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -3,15 +3,18 @@
     <app-header />
     <nuxt />
     <app-footer />
+    <app-offline-message v-if="$nuxt.isOffline" />
   </div>
 </template>
 <script>
 import AppFooter from '~/components/essentials/footer'
 import AppHeader from '~/components/essentials/header'
+import AppOfflineMessage from '~/components/essentials/offlineMessage'
 export default {
   components: {
     AppHeader,
     AppFooter,
+    AppOfflineMessage,
   }
 }
 </script>
