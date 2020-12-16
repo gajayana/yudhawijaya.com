68d8c3a0e338e6d831955f3c3727a8c78fe20e65
Yosef Yudha Wijaya <yosef@yudhawijaya.com>
2020-12-16 15:46:52 +0700

    added snowball

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,21 @@
 */
 module.exports = {
   theme: {
+    extend: {
+      animation: {
+        wiggle: 'wiggle 1s ease-in-out infinite'
+      },
+      keyframes: {
+        wiggle: {
+          '0%, 100%': {
+            transform: 'rotate(-3deg)'
+          },
+          '50%': {
+            transform: 'rotate(3deg)'
+          }
+        }
+      }
+    },
     fontFamily: {
       sans: [
         'Roboto',
