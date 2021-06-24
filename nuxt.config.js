76bb7a6a317206cf9681bcfb2ab8dbf300efb0b0
Yosef Yudha Wijaya <yosef@yudhawijaya.com>
2021-06-24 11:00:06 +0700

    updated dependencies

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,14 +1,18 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'nuxt-ts',
+    title: 'Muka',
+    titleTemplate: '%s – yudhawijaya.com',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { hid: 'description', name: 'description', content: '' },
+      { hid: 'twitter:card', name: 'twitter:card', content: 'summary' },
+      { hid: 'twitter:creator', name: 'twitter:creator', content: '@tuan_yudha' },
+      { hid: 'twitter:site', name: 'twitter:site', content: '@tuan_yudha' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/png', href: '/favicon.png' }
     ]
   },
 
@@ -27,14 +31,58 @@ export default {
   buildModules: [
     // https://go.nuxtjs.dev/typescript
     '@nuxt/typescript-build',
+    // https://composition-api.nuxtjs.org
+    '@nuxtjs/composition-api/module',
+    // https://github.com/nuxt-community/google-fonts-module
+    [
+      '@nuxtjs/google-fonts',
+      {
+        display: 'swap',
+        families: {
+          'Open+Sans': {
+            wght: [400, 700],
+            ital: [400, 700]
+          },
+          Roboto: [700]
+        }
+      }
+    ],
     // https://go.nuxtjs.dev/tailwindcss
-    '@nuxtjs/tailwindcss'
+    '@nuxtjs/tailwindcss',
+    [
+      '@nuxtjs/fontawesome',
+      {
+        icons: {
+          solid: ['faBars', 'faExternalLinkAlt', 'faLanguage', 'faTimes'],
+          brands: ['faGithub', 'faGitlab', 'faLinkedin', 'faTwitter']
+        }
+      }
+    ]
+
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios',
+    [
+      'storyblok-nuxt',
+      {
+        accessToken: process.env.STORYBLOK_TOKEN || '',
+        cacheProvider: 'memory'
+      }
+    ],
+    [
+      '@nuxtjs/markdownit',
+      // See https://github.com/markdown-it/markdown-it
+      {
+        injected: true
+      }
+    ],
+    // See https://github.com/nuxt-community/robots-module
+    '@nuxtjs/robots',
+    // See https://github.com/nuxt-community/sitemap-module
+    '@nuxtjs/sitemap',
     // https://go.nuxtjs.dev/pwa
     '@nuxtjs/pwa'
   ],
@@ -51,5 +99,12 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-  }
+    extend (config) {
+      config.node = {
+        fs: 'empty'
+      }
+    }
+  },
+
+  createRequire: 'jiti'
 }
