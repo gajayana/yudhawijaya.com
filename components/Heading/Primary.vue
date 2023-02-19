2d7ce1e12f95a39cbec05291551ffa48f9abf914
gajayana <yosef@yudhawijaya.com>
2023-02-19 17:35:18 +0700

    updated font size

--- components/Heading/Primary.vue
+++ components/Heading/Primary.vue
@@ -1,5 +1,5 @@
 <template>
-  <h1 class="font-sans font-semibold leading-tight mb-4 sm:mb-8 text-center text-4xl sm:text-6xl">
+  <h1 class="font-sans font-medium leading-tight mb-4 sm:mb-8 text-center text-3xl md:text-6xl lg:text-8xl">
     <slot/>
   </h1>
 </template>
--- components/Heading/Secondary.vue
+++ components/Heading/Secondary.vue
@@ -1,5 +1,5 @@
 <template>
-  <h2 class="flex font-medium leading-tight font-sans mb-4 lg:mb-8 text-center text-2xl lg:text-4xl">
+  <h2 class="flex font-medium leading-tight font-sans mb-4 lg:mb-8 text-center text-2xl md:text-4xl lg:text-6xl">
     <slot />
   </h2>
 </template>
