80d4d82f026f5f6445407d598c9087133925365a
Yosef Yudha Wijaya <yosef@yudhawijaya.com>
2024-09-05 14:35:35 +0700

    client only test

--- components/Button/LanguageSwitcher.vue
+++ components/Button/LanguageSwitcher.vue
@@ -5,16 +5,18 @@ const switchLocalePath = useSwitchLocalePath()
 </script>
 
 <template>
-  <div class="backdrop-blur-sm bg-[#f4f4f5]/50 flex overflow-hidden p-1 relative rounded-full">
-    <NuxtLink
-      v-for="language in locales"
-      :key="`switcher-${language.code}`"
-      :to="switchLocalePath(language.code)"
-      class="flex font-medium h-8 items-center justify-center no-underline outline-none active:outline-none focus:outline-none relative rounded-full text-sm text-black w-8"
-    >
-      {{ language.code.toUpperCase() }}
-    </NuxtLink>
-  </div>
+  <ClientOnly>
+    <div class="backdrop-blur-sm bg-[#f4f4f5]/50 flex overflow-hidden p-1 relative rounded-full">
+      <NuxtLink
+        v-for="language in locales"
+        :key="`switcher-${language.code}`"
+        :to="switchLocalePath(language.code)"
+        class="flex font-medium h-8 items-center justify-center no-underline outline-none active:outline-none focus:outline-none relative rounded-full text-sm text-black w-8"
+      >
+        {{ language.code.toUpperCase() }}
+      </NuxtLink>
+    </div>
+  </ClientOnly>
 </template>
 
 <style lang="postcss" scoped>
