e33c4d2c3ce8457ab91da5f66be0c57e1f38aa6d
gajayana <yosef@yudhawijaya.com>
2025-04-06 07:56:54 +0700

    added jobTitle field to the application metadata in nuxt.config.ts for enhanced user information

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -108,6 +108,7 @@ export default defineNuxtConfig({
       image: IMAGE_OF_ME,
       name: process.env.NUXT_PUBLIC_APP_NAME ?? "",
       sameAs: SCHEMA_PERSON_SAME_AS,
+      jobTitle: "Product Engineer",
     },
   },
 
